f7629e8f112fabb89dfa9031c0b1e386fb9c0fed
Aman Pratap <92034310+pratapaman@users.noreply.github.com>
2024-06-26 06:10:19 +0000

    config

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+
+const [backendData, setBackendData] = useState ([{}])
+
+useEffect(() => {
+  fetch("/api").then (
+    response => response.json()
+  ).then(
+    data => {
+      setBackendData(data)
+    }
+  )
+}, []) 
 
 function App(){
   return(
     <div>
+      {(typeof backendData.user === 'undefined')?(
+        <p>Loading...</p>
+      ):(
+        backendData.user.map((user, i) => (
+          <p key = {i}>{user}</p>
+        ))
+      )}
 
     </div>
   )
